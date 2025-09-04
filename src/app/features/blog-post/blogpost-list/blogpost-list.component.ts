7266d3d8692e0de3f5a2b6347ba50dae8bc61ea3
robertgruevski <robertgruevski@outlook.com>
2025-09-04 15:02:07 -0400

    UI - Get All BlogPosts

--- src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BlogPostService } from '../services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-blogpost-list',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './blogpost-list.component.html',
   styleUrl: './blogpost-list.component.css'
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
+
+  blogPosts$?: Observable<BlogPost[]>;;
+
+  constructor(private blogPostService: BlogPostService) { }
+
+  ngOnInit(): void {
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 
 }
