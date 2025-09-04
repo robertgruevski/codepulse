c326176a7687fb76cb83ef21b299aafb0a332e7b
robertgruevski <robertgruevski@outlook.com>
2025-09-03 20:33:47 -0400

    UI - Create UI component Edit-Category Component and pass route parameters

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { HomeComponent } from './core/components/home/home.component';
+import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 
 export const routes: Routes = [
-  { path: '' , component: HomeComponent, pathMatch: 'full'},
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'admin/categories', component: CategoryListComponent },
   { path: 'admin/categories/add', component: AddCategoryComponent },
+  { path: 'admin/categories/:id', component: EditCategoryComponent },
 ];
