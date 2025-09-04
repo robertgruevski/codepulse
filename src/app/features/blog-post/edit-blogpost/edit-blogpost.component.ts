2c9ed83ffc8372322d9b8a788c2177c100754fe0
robertgruevski <robertgruevski@outlook.com>
2025-09-04 17:25:02 -0400

    UI - Show Categories and Selected Categories

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
 import { FormsModule } from '@angular/forms';
 import { MarkdownModule } from 'ngx-markdown';
-import { DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { CategoryService } from '../../category/services/category.service';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-edit-blogpost',
-  imports: [FormsModule, MarkdownModule, DatePipe],
+  imports: [FormsModule, MarkdownModule, DatePipe, AsyncPipe],
   templateUrl: './edit-blogpost.component.html',
   styleUrl: './edit-blogpost.component.css',
 })
 export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
   model?: BlogPost;
+  categories$?: Observable<Category[]>;
+  selectedCategories?: string[];
   routeSubscription?: Subscription;
 
-  constructor(private route: ActivatedRoute, private blogPostService: BlogPostService) {}
+  constructor(private route: ActivatedRoute, private blogPostService: BlogPostService, private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.categoryService.getAllCategories();
+
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
@@ -29,6 +35,7 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
           this.blogPostService.getBlogPostById(this.id).subscribe({
             next: (response) => {
               this.model = response;
+              this.selectedCategories = response.categories.map(x => x.id);
             },
           });
         }
