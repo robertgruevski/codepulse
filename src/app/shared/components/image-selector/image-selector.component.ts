2a73bc3c89f2dcc5b6f0e0660626b7822e86d570
robertgruevski <robertgruevski@outlook.com>
2025-09-05 14:46:02 -0400

    UI - Create Image Service and Upload Image Form

--- src/app/shared/components/image-selector/image-selector.component.ts
+++ src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ImageService } from './image.service';
 
 @Component({
   selector: 'app-image-selector',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './image-selector.component.html',
-  styleUrl: './image-selector.component.css'
+  styleUrl: './image-selector.component.css',
 })
 export class ImageSelectorComponent {
+  private file?: File;
+  fileName: string = '';
+  title: string = '';
 
+  constructor(private imageService: ImageService) {}
+
+  onFileUploadChange(event: Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    this.file = element.files?.[0];
+  }
+
+  uploadImage(): void {
+    if (this.file && this.fileName !== '' && this.title !== '') {
+      this.imageService.uploadImage(this.file, this.fileName, this.title).subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+      });
+    }
+  }
 }
