107fc1dde3cc9df3fc23f9628854ab9ef03ce7cc
robertgruevski <robertgruevski@outlook.com>
2025-09-05 17:26:48 -0400

    UI - Display BlogDetails By URL

--- src/app/features/public/blog-details/blog-details.component.ts
+++ src/app/features/public/blog-details/blog-details.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BlogPostService } from '../../blog-post/services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../../blog-post/models/blog-post.model';
+import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
+import { MarkdownComponent } from "ngx-markdown";
 
 @Component({
   selector: 'app-blog-details',
-  imports: [],
+  imports: [CommonModule, AsyncPipe, DatePipe, MarkdownComponent],
   templateUrl: './blog-details.component.html',
-  styleUrl: './blog-details.component.css'
+  styleUrl: './blog-details.component.css',
 })
 export class BlogDetailsComponent implements OnInit {
-
   url: string | null = null;
+  blogPost$?: Observable<BlogPost>;
 
-  constructor(private route: ActivatedRoute) {
-
-  }
+  constructor(private route: ActivatedRoute, private blogPostService: BlogPostService) {}
 
   ngOnInit(): void {
-    this.route.paramMap
-    .subscribe({
+    this.route.paramMap.subscribe({
       next: (params) => {
         this.url = params.get('url');
-      }
-    })
+      },
+    });
 
-
+    if (this.url) {
+      this.blogPost$ = this.blogPostService.getBlogPostByUrlHandle(this.url);
+    }
   }
 }
