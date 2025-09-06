41f5dca3afce14314c38dc2750b556c179a3f6e8
robertgruevski <robertgruevski@outlook.com>
2025-09-06 13:59:07 -0400

    UI - Interceptors and Passing Auth Header Using Interceptor

--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -5,35 +5,48 @@ import { BlogPost } from '../models/blog-post.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { UpdateBlogPost } from '../models/update-blog-post.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BlogPostService {
 
-  constructor(private http: HttpClient) {}
-
-  createBlogPost(data: AddBlogPost): Observable<BlogPost> {
-    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts`, data);
-  }
-
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
+  
   getAllBlogPosts(): Observable<BlogPost[]> {
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/BlogPosts`);
   }
-
+  
   getBlogPostById(id: string): Observable<BlogPost> {
     return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts/${id}`);
   }
-
+  
   getBlogPostByUrlHandle(urlHandle: string): Observable<BlogPost> {
     return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts/${urlHandle}`);
   }
 
+  createBlogPost(data: AddBlogPost): Observable<BlogPost> {
+    return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts`, data, {
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
+  }
+
   updateBlogPost(id: string, updatedBlogPost: UpdateBlogPost): Observable<BlogPost> {
-    return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts/${id}`, updatedBlogPost);
+    return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts/${id}`, updatedBlogPost, {
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
   }
 
   deleteBlogPost(id: string): Observable<BlogPost> {
-    return this.http.delete<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts/${id}`);
+    return this.http.delete<BlogPost>(`${environment.apiBaseUrl}/api/BlogPosts/${id}`, {
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
   }
 }
