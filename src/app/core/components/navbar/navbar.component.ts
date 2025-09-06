e0d2403675cfffbd325925fe59596672e2dcc4a9
robertgruevski <robertgruevski@outlook.com>
2025-09-06 13:04:29 -0400

    UI - Implement Logout

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../features/auth/services/auth.service';
+import { User } from '../../../features/auth/models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent implements OnInit {
-  constructor(private authService: AuthService) { }
+  user?: User;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    this.authService.user()
-      .subscribe({
-        next: (response) => {
-          console.log(response);
-        }
-      })
+    this.authService.user().subscribe({
+      next: (response) => {
+        this.user = response;
+      },
+    });
+
+    this.user = this.authService.getUser();
   }
 
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
+  }
 }
