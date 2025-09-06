02111b9747d8ef417c587f80fd0b523cff0eb02c
robertgruevski <robertgruevski@outlook.com>
2025-09-06 15:10:25 -0400

    UI - Auth Guard and Implementation
    UI - Add Filtering to Categories Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,15 +8,16 @@ import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpo
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
 import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { authGuard } from './features/auth/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'blog/:url', component: BlogDetailsComponent },
-  { path: 'admin/categories', component: CategoryListComponent },
-  { path: 'admin/categories/add', component: AddCategoryComponent },
-  { path: 'admin/categories/:id', component: EditCategoryComponent },
-  { path: 'admin/blogposts', component: BlogpostListComponent },
-  { path: 'admin/blogposts/add', component: AddBlogpostComponent },
-  { path: 'admin/blogposts/:id', component: EditBlogpostComponent },
+  { path: 'admin/categories', component: CategoryListComponent, canActivate: [authGuard] },
+  { path: 'admin/categories/add', component: AddCategoryComponent, canActivate: [authGuard]  },
+  { path: 'admin/categories/:id', component: EditCategoryComponent, canActivate: [authGuard]  },
+  { path: 'admin/blogposts', component: BlogpostListComponent, canActivate: [authGuard]  },
+  { path: 'admin/blogposts/add', component: AddBlogpostComponent, canActivate: [authGuard]  },
+  { path: 'admin/blogposts/:id', component: EditBlogpostComponent, canActivate: [authGuard]  },
 ];
